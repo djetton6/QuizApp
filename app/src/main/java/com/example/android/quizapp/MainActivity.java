715a0c0959a32068cb55f0778c4cb6c91304994a
Desmend Jetton <djetton@iit.edu>
2018-06-22 23:57:39 -0500

    Finishing up Java code for quiz options.

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.android.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioButton stem_opt3,
+
 
 
     @Override
