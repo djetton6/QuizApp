794f9abf4a3fcccf2c8347d4563e80c0cb973ff1
Desmend Jetton <djetton@iit.edu>
2018-06-24 19:00:23 -0500

    Reformated code for readability. Also started adding ID's to all elements for using the onClick listener.

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -2,19 +2,25 @@ package com.example.android.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     RadioButton stem_opt3,
+    CheckBox stemCareer_1, stemCareer_2, stemCareer_3, stemCareer_4,
 
+    show();
 
-
+    Toast.makeText(context,text,duration).
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        stem_opt3 = (RadioButton) findViewById(R.id.stem_opt3);
     }
 
 }
